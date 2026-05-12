e117b37abcbddfd07d62a54b7d94f0c6887b9839
Ph-Alves <ph.costaalves@gmail.com>
2026-05-12 14:41:46 -0300

    Creating project and making Window with code

--- LearningUIKit/LearningUIKit/ViewController.swift
+++ LearningUIKit/LearningUIKit/ViewController.swift
@@ -5,22 +5,22 @@
 //  Created by Paulo Henrique Costa Alves on 11/05/26.
 //
 
-import Cocoa
+import UIKit
 
-class ViewController: NSViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    let campo = UITextField()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        campo.delegate = self
+        self.view.addSubview(campo)
         // Do any additional setup after loading the view.
     }
 
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
-
-
 }
 
