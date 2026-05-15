ab1f805d2ace5edbf059e772a46228c7de1b1115
Ph-Alves <ph.costaalves@gmail.com>
2026-05-15 16:32:10 -0300

    Learning to navigating via storyboard

---
+++ LearningUIKitStoryboard/LearningUIKitStoryboard/Controllers/PresentableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PresentableViewController.swift
+//  LearningUIKitStoryboard
+//
+//  Created by Paulo Henrique Costa Alves on 15/05/26.
+//
+
+import UIKit
+
+class PresentableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
